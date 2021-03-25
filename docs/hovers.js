5a7822492691642c3cb88d114c1fdb1c7f5604b3
Kelsey Anderson <kjanderson@cs.uchicago.edu>
2021-03-25 00:27:13 -0500

    updating portfolio main page:

---
+++ docs/hovers.js
@@ -0,0 +1,31 @@
+﻿window.onload = function () {
+
+    // Get the Object by ID
+
+    let svgs = document
+        .getElementsByTagName("object");
+
+
+    for (var i = 0; i < svgs.length; i++) {
+
+        console.log("loop 1: ", svgs[i]);
+        let doc = svgs[i].getSVGDocument();
+
+        console.log("get doc: ", doc);
+
+        let path = doc.getElementsByTagName("path");
+
+        console.log("get path: ", path);
+
+        path.addEventListener("onmouseover", function () {
+            path.setAttribute("fill", "blue");
+        });
+    }
+
+};
+
+
+
+
+
+ 
